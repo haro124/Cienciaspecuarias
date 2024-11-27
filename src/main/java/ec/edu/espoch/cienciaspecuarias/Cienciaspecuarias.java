8ac3b14d15616c95e54b1b2e8f90cddf9c5cae0d
haro124 <haroldcarreno1@gmail.com>
2024-11-27 16:09:28 -0500

    uwu

---
+++ src/main/java/ec/edu/espoch/cienciaspecuarias/Cienciaspecuarias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.cienciaspecuarias;
+
+/**
+ *
+ * @author Dylan Romero
+ */
+public class Cienciaspecuarias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
